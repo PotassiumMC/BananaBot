aed53c83dd46bf781b5f020cd460c631f20d2c6e
Noah van der Aa <ndvdaa@gmail.com>
2022-03-28 13:12:08 +0200

    Remove unnecessaary client creation in command deploy script

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,10 +1,8 @@
 const config = require('./config.json');
 
 const fs = require('node:fs');
-const { Client, Intents } = require('discord.js');
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v9');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 const commands = [];
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
@@ -18,15 +16,11 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '9' }).setToken(config.keys.discord);
 
-client.on('ready', () => {
-    console.log(`Registering commands as ${client.user.tag}!`);
-
-    rest.put(Routes.applicationCommands(client.user.id), { body: commands })
+rest.get('/users/@me').then((user) => {
+    console.log(`Registering commands as ${user.username}#${user.discriminator}!`);
+    rest.put(Routes.applicationCommands(user.id), { body: commands })
         .then(() => {
             console.log('Successfully registered application commands!');
-            client.destroy();
         })
         .catch(console.error);
-});
-
-client.login(config.keys.discord);
+}).catch(console.error);
