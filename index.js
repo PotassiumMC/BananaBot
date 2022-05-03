7232b0a7e6b05f2717b1d4d412e0879a8e785e6e
Noah van der Aa <ndvdaa@gmail.com>
2022-05-03 19:53:40 +0200

    Auto-upload text files to paste.gg

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const config = require('./config.json');
 const { getModrinthDownloadCounts, getCurseForgeDownloadCounts } = require('./utils/downloadCountUtils.js');
 
+const fetch = require('node-fetch');
 const fs = require('node:fs');
 const { Client, Collection, Intents } = require('discord.js');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 client.config = config;
 client.commands = new Collection();
@@ -15,6 +16,45 @@ for (const file of commandFiles) {
     console.log(`Discovered command: ${command.data.name}!`);
     client.commands.set(command.data.name, command);
 }
+
+client.on('messageCreate', async message => {
+    if (message.attachments.size === 0) return;
+
+    for (const [attachmentID, attachment] of message.attachments) {
+        if (!attachment.contentType.startsWith('text/plain')) continue;
+
+        const content = await (await fetch(attachment.url)).text();
+
+        const paste = await (await fetch('https://api.paste.gg/v1/pastes', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: `Attachment by ${message.author.tag} (${message.author.id})`,
+                visibility: 'unlisted',
+                files: [
+                    {
+                        name: attachment.name,
+                        content: {
+                            format: 'text',
+                            value: content
+                        }
+                    }
+                ]
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+                Authorization: `Key ${config.keys.pastegg}`
+            }
+        })).json();
+
+        if (paste.status !== 'success') return;
+
+        await message.reply({
+            allowedMentions: { users: [], roles: [] },
+            content: `${attachment.name} by <@${message.author.id}>: <https://paste.gg/${paste.result.id}>`
+        });
+    }
+});
 
 client.on('interactionCreate', async interaction => {
     if (!interaction.isCommand() && !interaction.isContextMenu() && interaction.member) return;
